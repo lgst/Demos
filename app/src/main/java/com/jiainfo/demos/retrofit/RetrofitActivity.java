545ae81dcff8e199b49a84511c8645c3b63548ef
lgst <1543117173@qq.com>
2018-03-01 16:51:52 +0800

    创建项目

---
+++ app/src/main/java/com/jiainfo/demos/retrofit/RetrofitActivity.java
@@ -0,0 +1,72 @@
+package com.jiainfo.demos.retrofit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.jiainfo.demos.R;
+
+import java.io.IOException;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import rx.Observable;
+import rx.Subscriber;
+
+/**
+ * @author lyg
+ */
+public class RetrofitActivity extends AppCompatActivity {
+
+    private TextView mTvText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retrofit);
+        initView();
+    }
+
+    public void getClick(View view) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("https://api.github.com/")
+                .build();
+        GitHubService service = retrofit.create(GitHubService.class);
+        Call<List<Repo>> repos = service.listRepos("lgst");
+        repos.enqueue(new Callback<List<Repo>>() {
+            @Override
+            public void onResponse(Call<List<Repo>> call, Response<List<Repo>> response) {
+                if (response.body() != null) {
+                    for (Repo repo : response.body()) {
+                        mTvText.append(repo.getName()+"\n");
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Repo>> call, Throwable t) {
+
+            }
+        });
+    }
+
+    private void initView() {
+        mTvText = findViewById(R.id.tv_text);
+    }
+
+    interface GitHubService {
+        @GET("users/{user}/repos")
+        Call<List<Repo>> listRepos(@Path("user") String user);
+
+    }
+}
---
+++ app/src/main/java/com/jiainfo/demos/rx/RxAndroidActivity.java
@@ -0,0 +1,18 @@
+package com.jiainfo.demos.rx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jiainfo.demos.R;
+
+/**
+ * @author lyg
+ */
+public class RxAndroidActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rx_android);
+    }
+}
